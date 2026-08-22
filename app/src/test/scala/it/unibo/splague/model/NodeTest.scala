e1fc5611ec8753ed306518512cd6d83132818b5b
eduardalexandrut <aeduardtoni@gmail.com>
2026-08-22 22:02:58 +0200

    test: add unit tests for Topology and NodeState

--- app/src/test/scala/it/unibo/splague/model/NodeTest.scala
+++ app/src/test/scala/it/unibo/splague/model/NodeTest.scala
@@ -4,7 +4,9 @@ import it.unibo.splague.model.NodeState.{Healthy, Immune}
 import it.unibo.splague.model.NodeType.Router
 import org.junit.runner.RunWith
 import org.scalatest.funsuite.AnyFunSuite
-import org.scalatest.matchers.should.Matchers.shouldBe
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.matchers.must.Matchers.contain
+import org.scalatest.matchers.should.Matchers.{should, shouldBe}
 import org.scalatestplus.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
@@ -41,3 +43,25 @@ class NodeTest extends AnyFunSuite:
     node.defenseLevel shouldBe 0.5
     node.state shouldBe NodeState.Healthy
     node.workload shouldBe 0.5
+
+  test("Topology should correctly store nodes and handle empty connections"):
+    val node1 = Node("node-01", NodeType.Router, 0.1, 0.2, NodeState.Infected, 0.5)
+    val node2 = Node("node-02", NodeType.MobileDevice, 0.0, 0.1, NodeState.Healthy, 0.1)
+
+    val nodesMap = Map("node-01" -> node1, "node-02" -> node2)
+    val topology = Topology(nodes = nodesMap, connections = Set.empty)
+
+    topology.nodes.size shouldBe 2
+    topology.nodes.contains("node-01") shouldBe true
+    topology.nodes("node-01").state shouldBe NodeState.Infected
+    topology.connections shouldBe Matchers.empty
+
+  test("NodeState enum should contain all expected states"):
+    val states = NodeState.values
+    states should contain allOf (
+      NodeState.Healthy,
+      NodeState.Infected,
+      NodeState.Quarantined,
+      NodeState.Immune,
+      NodeState.Destroyed
+    )
