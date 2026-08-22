fe54aa3775d348b53fe28363ac865e2eff84e167
eduardalexandrut <aeduardtoni@gmail.com>
2026-08-22 19:19:36 +0200

    feat: define NodeType, Node, NodeState, Topology

---
+++ app/src/main/scala/it/unibo/splague/model/Node.scala
@@ -0,0 +1,54 @@
+package it.unibo.splague.model
+
+sealed trait NodeType:
+  def detectionCoefficient: Double
+  def structuralVulnerability: Double
+
+object NodeType:
+  case object IoTDevice extends NodeType:
+    def detectionCoefficient = 0.3
+    def structuralVulnerability = 1.3
+
+  case object Workstation extends NodeType:
+    def detectionCoefficient = 1.5
+    def structuralVulnerability = 0.8
+
+  case object Router extends NodeType:
+    def detectionCoefficient = 0.3
+    def structuralVulnerability = 1.3
+
+  case object Server extends NodeType:
+    def detectionCoefficient = 0.3
+    def structuralVulnerability = 1.3
+
+  case object MobileDevice extends NodeType:
+    def detectionCoefficient = 0.3
+    def structuralVulnerability = 1.3
+
+enum NodeState:
+  case Healthy, Infected, Quarantined, Immune, Destroyed
+
+case class Node(
+    nodeId: String,
+    nodeType: NodeType,
+    patchLevel: Double,
+    defenseLevel: Double,
+    state: NodeState,
+    workload: Double
+)
+
+@SerialVersionUID(1L)
+case class Topology(
+    @transient nodes: Map[String, Node],
+    @transient connections: Set[Nothing]
+)
+
+//probabilitaContagio =
+//  clamp(
+//    virus.tratti.infettivita
+//      × collegamento.tipo.fattorePropagazione
+//      × (1 − nodoBersaglio.defenseLevel)
+//      × (1 − nodoBersaglio.patchLevel)
+//      × nodoBersaglio.tipo.vulnerabilitaStrutturale,
+//    0, 1
+//  )
