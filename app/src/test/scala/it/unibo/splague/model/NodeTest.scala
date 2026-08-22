9007b38e801d69facdc34f9d61bed0bc866ba604
eduardalexandrut <aeduardtoni@gmail.com>
2026-08-22 22:00:14 +0200

    test: add unit tests for Node

--- app/src/test/scala/it/unibo/splague/model/NodeTest.scala
+++ app/src/test/scala/it/unibo/splague/model/NodeTest.scala
@@ -1,5 +1,7 @@
 package it.unibo.splague.model
 
+import it.unibo.splague.model.NodeState.{Healthy, Immune}
+import it.unibo.splague.model.NodeType.Router
 import org.junit.runner.RunWith
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers.shouldBe
@@ -22,3 +24,20 @@ class NodeTest extends AnyFunSuite:
 
     NodeType.MobileDevice.detectionCoefficient shouldBe 0.3
     NodeType.MobileDevice.structuralVulnerability shouldBe 1.3
+
+  test("Node instance should be created with correct properties"):
+    val node = Node(
+      "node-01",
+      NodeType.Router,
+      0.2,
+      0.5,
+      Healthy,
+      0.5
+    )
+
+    node.nodeId shouldBe "node-01"
+    node.nodeType shouldBe NodeType.Router
+    node.patchLevel shouldBe 0.2
+    node.defenseLevel shouldBe 0.5
+    node.state shouldBe NodeState.Healthy
+    node.workload shouldBe 0.5
