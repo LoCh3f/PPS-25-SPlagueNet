9874e71e8a3fbe253fe95b2b82f80b9ab2bf1d75
eduardalexandrut <aeduardtoni@gmail.com>
2026-09-02 17:14:22 +0200

    feat: define DefenseRules object and boostDefense() function

---
+++ app/src/main/scala/it/unibo/splague/update/DefenseRules.scala
@@ -0,0 +1,11 @@
+package it.unibo.splague.update
+
+import it.unibo.splague.model.node.Node
+
+object DefenseRules:
+  private val DefenseBoostAmount: Double = 0.05
+  private val PatchBoostAmount: Double = 0.05
+  private val PatchCureProbability: Double = 0.5
+
+  def boostDefense(node: Node): Node =
+    node.copy(defenseLevel = math.min(1.0, node.defenseLevel + DefenseBoostAmount))
---
+++ app/src/test/scala/it/unibo/splague/update/DefenseRulesSuite.scala
@@ -0,0 +1,38 @@
+package it.unibo.splague.update
+
+import it.unibo.splague.model.node.{Node, NodeId, NodeState, NodeType}
+import org.junit.runner.RunWith
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatestplus.junit.JUnitRunner
+import org.scalatest.matchers.should.Matchers.*
+
+@RunWith(classOf[JUnitRunner])
+class DefenseRulesSuite extends AnyFunSuite:
+  test("DefenseBoost increases the defense level of healthy nodes up to 1.0"):
+    val node = Node(
+      nodeId = NodeId.of("n1").fold(err => fail(s"Failed to create NodeId: $err"), identity),
+      nodeType = NodeType.Workstation,
+      patchLevel = 0.0,
+      defenseLevel = 0.8,
+      state = NodeState.Healthy,
+      workload = 0.0,
+      vectors = Set()
+    )
+
+    val boostedNode = DefenseRules.boostDefense(node)
+
+    boostedNode.defenseLevel shouldBe 0.85 +- 0.0001
+
+  test("DefenseBoost clamps defense level at 1.0"):
+    val node = Node(
+      nodeId = NodeId.of("n1").fold(err => fail(s"Failed to create NodeId: $err"), identity),
+      nodeType = NodeType.Workstation,
+      patchLevel = 0.0,
+      defenseLevel = 0.9,
+      state = NodeState.Healthy,
+      workload = 0.0,
+      vectors = Set()
+    )
+
+    val boostedNode = DefenseRules.boostDefense(node)
+    boostedNode.defenseLevel shouldBe 0.95 +- 0.0001
