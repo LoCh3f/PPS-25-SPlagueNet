aa6e06816ba324337de240e321417de8d4499440
eduardalexandrut <aeduardtoni@gmail.com>
2026-09-02 16:22:33 +0200

    feat: define isApplicable function in Countermeasures

--- app/src/main/scala/it/unibo/splague/model/countermeasures/Countermeasures.scala
+++ app/src/main/scala/it/unibo/splague/model/countermeasures/Countermeasures.scala
@@ -1,4 +1,11 @@
 package it.unibo.splague.model.countermeasures
 
+import it.unibo.splague.model.node.NodeType
+import it.unibo.splague.model.node.NodeType.IoTDevice
+
 enum Countermeasures:
   case DefenseBoost, Firewall, Isolation, Patch
+
+  def isApplicableTo(nodeType: NodeType): Boolean = this match
+    case Patch | DefenseBoost => nodeType != IoTDevice
+    case _                    => true
--- app/src/test/scala/it/unibo/splague/model/countermeasures/CountermeasuresSuite.scala
+++ app/src/test/scala/it/unibo/splague/model/countermeasures/CountermeasuresSuite.scala
@@ -6,9 +6,10 @@ import it.unibo.splague.model.countermeasures.Countermeasures.{
   Isolation,
   Patch
 }
+import it.unibo.splague.model.node.NodeType
 import org.junit.runner.RunWith
 import org.scalatest.funsuite.AnyFunSuite
-import org.scalatest.matchers.must.Matchers.contain
+import org.scalatest.matchers.must.Matchers.{be, contain}
 import org.scalatest.matchers.should.Matchers.should
 import org.scalatestplus.junit.JUnitRunner
 
@@ -22,3 +23,10 @@ class CountermeasuresSuite extends AnyFunSuite:
       Isolation,
       Patch
     )
+
+  test("Patch and DefenseBoost should not be available for IoT device node type"):
+    val nodeType = NodeType.IoTDevice
+    val isDefenseBoostApplicable = DefenseBoost.isApplicableTo(nodeType)
+    val isPatchApplicable = Patch.isApplicableTo(nodeType)
+
+    (isDefenseBoostApplicable, isPatchApplicable) should be(false, false)
