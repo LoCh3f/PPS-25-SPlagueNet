683268774aa9b49f2edf73f35f4939e1051048a3
eduardalexandrut <aeduardtoni@gmail.com>
2026-09-03 16:55:03 +0200

    feat: add patchBoostAmount,defenseBoostAmount,patchCureProbability fields in CountermeasureConfig

--- app/src/main/scala/it/unibo/splague/model/countermeasures/CountermeasureConfig.scala
+++ app/src/main/scala/it/unibo/splague/model/countermeasures/CountermeasureConfig.scala
@@ -2,15 +2,43 @@ package it.unibo.splague.model.countermeasures
 
 case class CountermeasureConfig(
     activeCountermeasures: Set[Countermeasures],
-    countermeasureLevels: Map[Double, Countermeasures]
+    countermeasureLevels: Map[Double, Countermeasures],
+    patchBoostAmount: Double,
+    defenseBoostAmount: Double,
+    patchCureProbability: Double
 )
 object CountermeasureConfig:
-  val empty: CountermeasureConfig = CountermeasureConfig(Set.empty, Map.empty).toOption.get
+  val defaultPatchBoostAmount: Double = 0.05
+  val defaultDefenseBoostAmount: Double = 0.05
+  val defaultPatchCureProbability: Double = 0.5
+  val empty: CountermeasureConfig = CountermeasureConfig(
+    Set.empty,
+    Map.empty,
+    defaultPatchBoostAmount,
+    defaultDefenseBoostAmount,
+    defaultPatchCureProbability
+  ).toOption.get
 
   def apply(
-      activeCountermeasures: Set[Countermeasures],
-      countermeasureLevels: Map[Double, Countermeasures]
+      activeCountermeasures: Set[Countermeasures] = Set.empty,
+      countermeasureLevels: Map[Double, Countermeasures] = Map.empty,
+      patchBoostAmount: Double = defaultPatchBoostAmount,
+      defenseBoostAmount: Double = defaultDefenseBoostAmount,
+      patchCureProbability: Double = defaultPatchCureProbability
   ): Either[String, CountermeasureConfig] =
     if countermeasureLevels.keys.exists(threshold => threshold < 0.0 || threshold > 1.0) then
       Left("Thresholds in countermeasureLevels must be between 0.0 and 1.0")
-    else Right(new CountermeasureConfig(activeCountermeasures, countermeasureLevels))
+    else if defenseBoostAmount < 0.0 || defenseBoostAmount > 1.0 ||
+      patchBoostAmount < 0.0 || patchBoostAmount > 1.0 ||
+      patchCureProbability < 0.0 || patchCureProbability > 1.0
+    then Left("Boost amounts and cure probability must be between 0.0 and 1.0")
+    else
+      Right(
+        new CountermeasureConfig(
+          activeCountermeasures,
+          countermeasureLevels,
+          patchBoostAmount,
+          defenseBoostAmount,
+          patchCureProbability
+        )
+      )
