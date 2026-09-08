b8b6303fc420aaec66d49037844dadeb9693c215
eduardalexandrut <aeduardtoni@gmail.com>
2026-09-09 00:06:22 +0200

    feat: define FirewallEvent and test suite

--- app/src/main/scala/it/unibo/splague/simulation/event/Defense.scala
+++ app/src/main/scala/it/unibo/splague/simulation/event/Defense.scala
@@ -1,10 +1,11 @@
 package it.unibo.splague.simulation.event
 
-import it.unibo.splague.model.countermeasures.Countermeasures.Isolation
+import it.unibo.splague.model.countermeasures.Countermeasures.{Firewall, Isolation}
 import it.unibo.splague.model.node.NodeId.NodeId
 import it.unibo.splague.model.node.{Node, NodeState, Topology}
 import it.unibo.splague.simulation.Scenario
 import it.unibo.splague.simulation.event.SimulationEvents.{Event, TopologyUpdateMixin}
+import it.unibo.splague.update.FirewallPolicy
 
 object Defense:
   private def cutEdgesOf(topology: Topology, nodes: Set[Node]): Topology =
@@ -16,13 +17,14 @@ object Defense:
     override def apply(scenario: Scenario): Scenario =
       val config = scenario.countermeasureConfig
 
-      if !config.activeCountermeasures.contains(Isolation) then scenario
+      if config.activeCountermeasures.contains(Isolation) then scenario
       else
         val criteria = config.isolationCriteria
         val targets = scenario.topology.infectedNodes().filter(criteria.matches).toSet
 
         if targets.isEmpty then scenario
         else
+          val updatedActive = config.activeCountermeasures + Isolation
           val updatedNodes = targets.foldLeft(scenario.topology.nodes) { (acc, node) =>
             acc.updated(node.nodeId.value, node.copy(state = NodeState.Quarantined))
           }
@@ -30,4 +32,23 @@ object Defense:
           // Remove connections
           val topologyWithoutEdges =
             cutEdgesOf(scenario.topology.copy(nodes = updatedNodes), targets)
-          scenario.copy(topology = topologyWithoutEdges)
+          scenario.copy(
+            topology = topologyWithoutEdges,
+            countermeasureConfig = config.copy(activeCountermeasures = updatedActive)
+          )
+
+  object FirewallEvent extends Event:
+    override def apply(scenario: Scenario): Scenario =
+      val config = scenario.countermeasureConfig
+
+      if config.activeCountermeasures.contains(Firewall) then scenario
+      else
+        val updatedActive = config.activeCountermeasures + Firewall
+        val updatedPolicy = config.firewallPolicy.merge(FirewallPolicy.defaultPolicy)
+
+        scenario.copy(
+          countermeasureConfig = config.copy(
+            activeCountermeasures = updatedActive,
+            firewallPolicy = updatedPolicy
+          )
+        )
