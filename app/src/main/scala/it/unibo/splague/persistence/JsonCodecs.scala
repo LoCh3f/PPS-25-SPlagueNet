a2c7a58dab1d69cbc2d84cd219724a7400f61cd3
eduardalexandrut <aeduardtoni@gmail.com>
2026-09-17 18:21:33 +0200

    feat: define custom json decoder for NodeState and generic json decoder

--- app/src/main/scala/it/unibo/splague/persistence/JsonCodecs.scala
+++ app/src/main/scala/it/unibo/splague/persistence/JsonCodecs.scala
@@ -6,6 +6,7 @@ import io.circe.syntax.*
 import io.circe.parser.decode
 import io.circe.generic.semiauto.*
 import it.unibo.splague.model.node.NodeState
+import it.unibo.splague.model.node.NodeState.{Destroyed, Healthy, Immune, Infected, Quarantined}
 import it.unibo.splague.persistence.{Encoder, FileFormat}
 
 object JsonCodecs:
@@ -17,6 +18,24 @@ object JsonCodecs:
   given Encoder[NodeState, FileFormat.Json] with
     override def encode(a: NodeState): String = s"\"$a\""
 
+  // Custom decoder for sealed traits, enums
+  given Decoder[NodeState, FileFormat.Json] with
+    override def decode(raw: String): Either[PersistenceError, NodeState] =
+      raw.trim.stripPrefix("\"").stripSuffix("\"") match
+        case "Healthy"     => Right(Healthy)
+        case "Infected"    => Right(Infected)
+        case "Quarantined" => Right(Quarantined)
+        case "Immune"      => Right(Immune)
+        case "Destroyed"   => Right(Destroyed)
+        case other         => Left(PersistenceError.Parsing(s"Unknown NodeState: $other"))
+
   // Generic encoder for json objects
   given [A](using cEncoder: CirceEncoder[A]): Encoder[A, FileFormat.Json] with
     override def encode(a: A): String = a.asJson.noSpaces
+
+  // Generic decoder
+  given [A](using cDecoder: CirceDecoder[A]): Decoder[A, FileFormat.Json] with
+    override def decode(raw: String): Either[PersistenceError, A] =
+      io.circe.parser.decode[A](raw) match
+        case Right(value) => Right(value)
+        case Left(error)  => Left(PersistenceError.Parsing(error.getMessage))
--- app/src/test/scala/it/unibo/splague/update/CodecSuite.scala
+++ app/src/test/scala/it/unibo/splague/update/CodecSuite.scala
@@ -8,7 +8,7 @@ import io.circe.syntax.*
 import io.circe.parser.*
 import io.circe.Codec
 import it.unibo.splague.model.node.NodeState
-import it.unibo.splague.persistence.{Encoder, FileFormat}
+import it.unibo.splague.persistence.{Decoder, Encoder, FileFormat, PersistenceError}
 import org.scalatest.matchers.should.Matchers
 import it.unibo.splague.persistence.JsonCodecs.given
 
@@ -23,3 +23,25 @@ final class CodecSuite extends AnyFunSuite with Matchers:
 
     val rawJson = encoder.encode(nodeState)
     rawJson shouldBe "\"Healthy\""
+
+  test("A JSON decoder should correctly decode a JSON NodeState object into a NodeState"):
+    val json = "\"Healthy\""
+
+    val decoder = summon[Decoder[NodeState, FileFormat.Json]]
+
+    val state = decoder.decode(json)
+    state shouldBe Right(NodeState.Healthy)
+
+  test(
+    "A JSON decoder should return a Persistence.Parsing error when an unknown NodeState is encountered"
+  ):
+    val json = "\"Unknown\""
+
+    val decoder = summon[Decoder[NodeState, FileFormat.Json]]
+
+    val state = decoder.decode(json)
+
+    state.isLeft shouldBe true
+    state.left.toOption.get match
+      case PersistenceError.Parsing(_) => succeed
+      case other                       => fail(s"Expected Parsing error, got $other")
