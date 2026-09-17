08447c42a97097340f10a374d54ffe96b677f6f6
eduardalexandrut <aeduardtoni@gmail.com>
2026-09-17 19:59:33 +0200

    feat: define custom json encoder/decoder for Map[Node, Double]

--- app/src/test/scala/it/unibo/splague/update/CodecSuite.scala
+++ app/src/test/scala/it/unibo/splague/update/CodecSuite.scala
@@ -2,13 +2,10 @@ package it.unibo.splague.update
 
 import org.junit.runner.RunWith
 import org.scalatest.funsuite.AnyFunSuite
-import org.scalatest.matchers.should.Matchers
 import org.scalatestplus.junit.JUnitRunner
-import io.circe.syntax.*
-import io.circe.parser.*
-import io.circe.Codec
 import it.unibo.splague.model.Awareness
-import it.unibo.splague.model.node.{NodeState, NodeType}
+import it.unibo.splague.model.node.NodeId.NodeId
+import it.unibo.splague.model.node.{Node, NodeId, NodeState, NodeType}
 import it.unibo.splague.persistence.{Decoder, Encoder, FileFormat, PersistenceError}
 import org.scalatest.matchers.should.Matchers
 import it.unibo.splague.persistence.JsonCodecs.given
@@ -105,3 +102,59 @@ final class CodecSuite extends AnyFunSuite with Matchers:
     state.left.toOption.get match
       case PersistenceError.Parsing(_) => succeed
       case other                       => fail(s"Expected Parsing error, got $other")
+
+  test("NodeId should be correctly encoded to a clean JSON string"):
+    val nodeId = NodeId.of("node-1").toOption.get
+    val encoder = summon[Encoder[NodeId, FileFormat.Json]]
+
+    encoder.encode(nodeId) shouldBe "\"node-1\""
+
+  test("NodeId should be correctly decoded from a clean JSON string"):
+    val json = "\"node-1\""
+    val decoder = summon[Decoder[NodeId, FileFormat.Json]]
+
+    val result = decoder.decode(json)
+
+    result shouldBe Right(NodeId.of("node-1").toOption.get)
+    result.toOption.get.value shouldBe "node-1"
+
+  test(
+    "A JSON decoder should return a Persistence.Parsing error when an invalid NodeId (e.g., containing whitespace) is encountered"
+  ):
+    val json = "\"node 1\""
+    val decoder = summon[Decoder[NodeId, FileFormat.Json]]
+
+    val state = decoder.decode(json)
+
+    state.isLeft shouldBe true
+    println(state.left.toOption.get)
+    state.left.toOption.get match
+      case PersistenceError.Parsing(_) => succeed
+      case other                       => fail(s"Expected Parsing error, got $other")
+
+  test("Map[Node, Double] should be correctly encoded to JSON list of pairs and decoded back"):
+    val nodeId = NodeId.of("node-1").toOption.get
+    val node = Node.test(
+      nodeId = nodeId,
+      nodeType = NodeType.Server,
+      state = NodeState.Healthy
+    )
+
+    val sampleMap: Map[Node, Double] = Map(node -> 0.75)
+
+    val encoder = summon[Encoder[Map[Node, Double], FileFormat.Json]]
+    val decoder = summon[Decoder[Map[Node, Double], FileFormat.Json]]
+
+    // Encoding test
+    val jsonString = encoder.encode(sampleMap)
+    jsonString should not be empty
+
+    // Decoding test
+    val result = decoder.decode(jsonString)
+    println(result)
+
+    result.isRight shouldBe true
+    val decodedMap = result.toOption.get
+    decodedMap should have size 1
+    decodedMap.head._2 shouldBe 0.75
+    decodedMap.head._1.nodeId.value shouldBe "node-1"
