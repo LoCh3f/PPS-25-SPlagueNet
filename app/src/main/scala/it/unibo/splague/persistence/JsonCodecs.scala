11077f9fe56954fd60070dfbfec13b6fa85577cb
eduardalexandrut <aeduardtoni@gmail.com>
2026-09-17 19:59:33 +0200

    feat: define custom json encoder/decoder for Map[Node, Double]

--- app/src/main/scala/it/unibo/splague/persistence/JsonCodecs.scala
+++ app/src/main/scala/it/unibo/splague/persistence/JsonCodecs.scala
@@ -5,62 +5,82 @@ import io.circe.{Codec as CirceCodec, Decoder as CirceDecoder, Encoder as CirceE
 import io.circe.syntax.*
 import io.circe.parser.decode
 import io.circe.generic.semiauto.*
+import io.circe.generic.auto.deriveDecoder
+import io.circe.generic.auto.deriveEncoder
 import it.unibo.splague.model.Awareness
-import it.unibo.splague.model.node.{NodeState, NodeType}
+import it.unibo.splague.model.node.NodeId.NodeId
+import it.unibo.splague.model.node.{Node, NodeId, NodeState, NodeType}
 import it.unibo.splague.model.node.NodeState.{Destroyed, Healthy, Immune, Infected, Quarantined}
 import it.unibo.splague.persistence.{Encoder, FileFormat}
 
 object JsonCodecs:
 
-  // Circe config
+  // --- Circe Configuration ---
   given Configuration = Configuration.default
 
-  // Custom encoders for atomic types
-  given Encoder[NodeState, FileFormat.Json] with
-    override def encode(a: NodeState): String = s"\"$a\""
+  // CIRCE ENCODERS/DECODERS
 
-  given Encoder[NodeType, FileFormat.Json] with
-    override def encode(a: NodeType): String = s"\"$a\""
+  // NodeId (with validation of NodeId.of)
+  given nodeIdCirceEncoder: CirceEncoder[NodeId] =
+    CirceEncoder.encodeString.contramap(_.value)
 
-  given Encoder[Awareness, FileFormat.Json] with
-    override def encode(a: Awareness): String = a.value.toString
+  given nodeIdCirceDecoder: CirceDecoder[NodeId] =
+    CirceDecoder.decodeString.emap { raw =>
+      NodeId.of(raw) match
+        case Right(id) => Right(id)
+        case Left(err) => Left(err)
+    }
 
-  // Custom decoders for atomic types
-  given Decoder[NodeState, FileFormat.Json] with
-    override def decode(raw: String): Either[PersistenceError, NodeState] =
-      raw.trim.stripPrefix("\"").stripSuffix("\"") match
-        case "Healthy"     => Right(Healthy)
-        case "Infected"    => Right(Infected)
-        case "Quarantined" => Right(Quarantined)
-        case "Immune"      => Right(Immune)
-        case "Destroyed"   => Right(Destroyed)
-        case other         => Left(PersistenceError.Parsing(s"Unknown NodeState: $other"))
+  // Awareness (with validation in range [0, 1])
+  given awarenessCirceEncoder: CirceEncoder[Awareness] =
+    CirceEncoder.encodeDouble.contramap(_.value)
 
-  given Decoder[NodeType, FileFormat.Json] with
-    override def decode(raw: String): Either[PersistenceError, NodeType] =
-      raw.trim.stripPrefix("\"").stripSuffix("\"") match
-        case "Workstation"  => Right(NodeType.Workstation)
-        case "Server"       => Right(NodeType.Server)
-        case "Router"       => Right(NodeType.Router)
-        case "IoTDevice"    => Right(NodeType.IoTDevice)
-        case "MobileDevice" => Right(NodeType.MobileDevice)
-        case other          => Left(PersistenceError.Parsing(s"Unknown NodeType: $other"))
+  given awarenessCirceDecoder: CirceDecoder[Awareness] =
+    CirceDecoder.decodeDouble.emap { d =>
+      Awareness(d) match
+        case Right(a)  => Right(a)
+        case Left(err) => Left(err)
+    }
 
-  given Decoder[Awareness, FileFormat.Json] with
-    override def decode(raw: String): Either[PersistenceError, Awareness] =
-      raw.trim.toDoubleOption match
-        case Some(doubleVal) =>
-          Awareness(doubleVal) match
-            case Right(awareness) => Right(awareness)
-            case Left(err)        => Left(PersistenceError.Parsing(err))
-        case None =>
-          Left(PersistenceError.Parsing(s"Invalid number format for Awareness: $raw"))
+  // NodeState
+  given nodeStateCirceEncoder: CirceEncoder[NodeState] =
+    CirceEncoder.encodeString.contramap(_.toString)
 
-  // Generic encoder for json objects
+  given nodeStateCirceDecoder: CirceDecoder[NodeState] =
+    CirceDecoder.decodeString.emap {
+      case "Healthy"     => Right(NodeState.Healthy)
+      case "Infected"    => Right(NodeState.Infected)
+      case "Quarantined" => Right(NodeState.Quarantined)
+      case "Immune"      => Right(NodeState.Immune)
+      case "Destroyed"   => Right(NodeState.Destroyed)
+      case other         => Left(s"Unknown NodeState: $other")
+    }
+
+  // NodeType
+  given nodeTypeCirceEncoder: CirceEncoder[NodeType] =
+    CirceEncoder.encodeString.contramap(_.toString)
+
+  given nodeTypeCirceDecoder: CirceDecoder[NodeType] =
+    CirceDecoder.decodeString.emap {
+      case "Workstation"  => Right(NodeType.Workstation)
+      case "Server"       => Right(NodeType.Server)
+      case "Router"       => Right(NodeType.Router)
+      case "IoTDevice"    => Right(NodeType.IoTDevice)
+      case "MobileDevice" => Right(NodeType.MobileDevice)
+      case other          => Left(s"Unknown NodeType: $other")
+    }
+
+  // Map[Node, Double] (serialization with list of tuples)
+  given mapNodeDoubleEncoder: CirceEncoder[Map[Node, Double]] =
+    CirceEncoder.encodeList[(Node, Double)].contramap(_.toList)
+
+  given mapNodeDoubleDecoder: CirceDecoder[Map[Node, Double]] =
+    CirceDecoder.decodeList[(Node, Double)].map(_.toMap)
+
+  // Generic json encoders/decoders that use CirceEncoders/Decoders
   given [A](using cEncoder: CirceEncoder[A]): Encoder[A, FileFormat.Json] with
-    override def encode(a: A): String = a.asJson.noSpaces
+    override def encode(a: A): String = cEncoder(a).noSpaces
 
-  // Generic decoder
   given [A](using cDecoder: CirceDecoder[A]): Decoder[A, FileFormat.Json] with
     override def decode(raw: String): Either[PersistenceError, A] =
       io.circe.parser.decode[A](raw) match
