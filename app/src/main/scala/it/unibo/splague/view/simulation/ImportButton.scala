063b2f55a79314bab2e0964c1f86018439137001
eduardalexandrut <aeduardtoni@gmail.com>
2026-09-18 20:04:05 +0200

    feat: define logic to import a scenario from file

--- app/src/main/scala/it/unibo/splague/view/simulation/ImportButton.scala
+++ app/src/main/scala/it/unibo/splague/view/simulation/ImportButton.scala
@@ -1,16 +1,28 @@
 package it.unibo.splague.view.simulation
 
-import it.unibo.splague.persistence.FileFormat
+import it.unibo.splague.persistence.{ExportPaths, FileFormat}
 import it.unibo.splague.update.Msg
 
-import javax.swing.{JButton, JMenuItem, JPopupMenu}
+import java.io.File
+import javax.swing.filechooser.FileNameExtensionFilter
+import javax.swing.{JButton, JFileChooser, JMenuItem, JPopupMenu}
 
 object ImportButton:
   def apply(dispatch: Msg => Unit): JButton =
     val importMenu = new JPopupMenu()
 
     val jsonItem = new JMenuItem("json")
-    jsonItem.addActionListener(_ => dispatch(Msg.ExportScenario(FileFormat.Json)))
+    jsonItem.addActionListener(_ =>
+      val fileChooser = new JFileChooser(ExportPaths.baseDirectory.toFile)
+
+      fileChooser.setFileFilter(new FileNameExtensionFilter("JSON Files", "json"))
+
+      val result = fileChooser.showOpenDialog(null)
+
+      if result == JFileChooser.APPROVE_OPTION then
+        val selectedFile: File = fileChooser.getSelectedFile
+        dispatch(Msg.ImportScenario(FileFormat.Json, selectedFile.toPath))
+    )
 
     importMenu.add(jsonItem)
 
