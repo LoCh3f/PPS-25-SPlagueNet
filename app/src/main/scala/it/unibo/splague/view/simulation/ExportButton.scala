79e0634bf64dff42514368471b59c2fa6b1c43b6
eduardalexandrut <aeduardtoni@gmail.com>
2026-09-18 18:48:53 +0200

    feat: add format field in ExportScenario msg in order to call the right repository

--- app/src/main/scala/it/unibo/splague/view/simulation/ExportButton.scala
+++ app/src/main/scala/it/unibo/splague/view/simulation/ExportButton.scala
@@ -1,5 +1,6 @@
 package it.unibo.splague.view.simulation
 
+import it.unibo.splague.persistence.FileFormat
 import it.unibo.splague.update.Msg
 
 import javax.swing.{JButton, JMenuItem, JPopupMenu}
@@ -10,11 +11,13 @@ object ExportButton:
     val exportMenu = new JPopupMenu()
 
     val jsonItem = new JMenuItem("json")
-    jsonItem.addActionListener(_ => dispatch(Msg.ExportScenario))
+    jsonItem.addActionListener(_ => dispatch(Msg.ExportScenario(FileFormat.Json)))
 
     val txtItem = new JMenuItem("txt")
+    jsonItem.addActionListener(_ => dispatch(Msg.ExportScenario(FileFormat.Txt)))
 
-    val ymlItem = new JMenuItem("yml")
+    val ymlItem = new JMenuItem("html")
+    jsonItem.addActionListener(_ => dispatch(Msg.ExportScenario(FileFormat.Html)))
 
     exportMenu.add(jsonItem)
     exportMenu.add(txtItem)
